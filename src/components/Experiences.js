6516b03a95ba42a6f8a4377ec192c3311116f806
JohnRob1 <robin557@purdue.edu>
2024-05-15 21:57:17 -0400

    fisher experience added

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -1,6 +1,7 @@
 import styles from '../styles/Experiences.module.css';
 import global from '../styles/Global.module.css';
 import dannarPic from '../assets/dannarPic.png';
+import fisherPic from '../assets/fisherPic.png';
 
 function Picture({ pic }) {
     return (
@@ -43,8 +44,19 @@ export default function Experiences() {
             <Tile 
                 title={'Software Engineer Intern at Dannar'}
                 date={'June 2024 - August 2024'}
-                descr={'Writing software with IQAN for the MPS.'}
+                descr={'I am going to develop software using IQAN for Dannar!'}
                 pictures={[dannarPic]}
+            />
+            <Tile 
+                title={'Software Consultant for Fisher Packing'}
+                date={'May 2023 - August 2023'}
+                descr={'I was responsible for getting all of Fisher Packing\'s ' +
+                    'inventory digitalized. This was needed to streamline quality ' +
+                    'assurance practices for all of their products. By the end of ' + 
+                    'summer, I developed an GS1-128 barcode decoding extension for Google Sheets, ' + 
+                    'input all inventory and recipe data into an inventory tracking software, ' + 
+                    'and used Google Forms to digitalize the quality assurance logs.'}
+                pictures={[fisherPic]}
             />
         </div>
     );
