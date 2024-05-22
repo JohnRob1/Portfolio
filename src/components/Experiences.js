083fb34b9ac73bb66da3949e36b559cc4f8c7472
JohnRob1 <robin557@purdue.edu>
2024-05-22 17:47:34 -0400

    global added back, Tile.js started

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -10,20 +10,20 @@ export default function Experiences() {
             <div className={global.title}>
                 EXPERIENCES
             </div>
-            <Tile 
+            <Tile
                 title='Software Engineer Intern at Dannar'
                 date='June 2024 - August 2024'
                 descr='I am going to develop software using IQAN for Dannar!'
                 pictures={[dannarPic]}
             />
-            <Tile 
+            <Tile
                 title='Software Consultant for Fisher Packing'
                 date='May 2023 - August 2023'
                 descr={'I was responsible for getting all of Fisher Packing\'s ' +
                     'inventory digitalized. This was needed to streamline quality ' +
-                    'assurance practices for all of their products. By the end of ' + 
-                    'summer, I developed an GS1-128 barcode decoding extension for Google Sheets, ' + 
-                    'input all inventory and recipe data into an inventory tracking software, ' + 
+                    'assurance practices for all of their products. By the end of ' +
+                    'summer, I developed an GS1-128 barcode decoding extension for Google Sheets, ' +
+                    'input all inventory and recipe data into an inventory tracking software, ' +
                     'and used Google Forms to digitalize the quality assurance logs.'}
                 pictures={[fisherPic]}
             />
