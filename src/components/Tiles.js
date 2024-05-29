6a4f4cb34ba16673a8839a3009571be4f17cb32d
JohnRob1 <robin557@purdue.edu>
2024-05-28 20:02:12 -0400

    added tiles.js

---
+++ src/components/Tiles.js
@@ -0,0 +1,57 @@
+import styles from '../styles/Tiles.module.css';
+import global from '../styles/Global.module.css';
+import * as React from "react";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+function Picture({ pic }) {
+    return (
+        <>
+            <img className={global.picture}
+                src={pic}
+                alt={pic}
+            />
+        </>
+    );
+}
+
+function Link({ link }) {
+    const title = link.substring(0, link.indexOf(':'));
+    const addr = link.substring(link.indexOf(':') + 1);
+
+    return (
+        <>
+            <a className={styles.link} href={addr}>{title}</a>
+        </>
+    );
+}
+
+export default function Tile({ title, date, desc, pics, links }) {
+    return (
+        <div className={styles.tile}>
+            <div className={styles.titleAndDate}>
+                <div className={styles.title}>
+                    {title}
+                </div>
+                <div className={styles.date}>
+                    {date}
+                </div>
+            </div>
+            <p className={styles.desc}>
+                {desc}
+            </p>
+            <div className={styles.picsAndLinks}>
+                <div className={styles.links}>
+                    {links?.map((link) => (
+                        <Link key={link} link={link} />
+                    ))}
+                </div>
+                <div className={styles.pics}>
+                    {pics?.map((pic) => (
+                        <Picture key={pic} pic={pic} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
