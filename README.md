5bf895bbd4137dca51362d2333460ede1992871a
JohnRob1 <robin557@purdue.edu>
2024-05-30 23:26:44 -0400

    increased animations' performance! Added more content about projects

--- README.md
+++ README.md
@@ -1,70 +1,11 @@
-# Getting Started with Create React App
+# My Software Development Portfolio
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+I built this portfolio with the intention of it being an extension of my resume.
+Throughout college, I did a lot of extracurricular work that I believe to show
+a well-roundedness. Also, I was exposed to React during my hackathon project, and
+ended up really enjoying it, so I thought I'd keep that going.
 
-## Available Scripts
-
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+This website is very minimal, with framer-motion being the only library I used
+during development. While I've used tailwind-css before, I found it to make
+the code very cluttered a not very readable. I really enjoyed a vanilla CSS
+experience, keeping all styling in separate files.
--- src/components/Creations.js
+++ src/components/Creations.js
@@ -1,39 +1,80 @@
 import Tiles from './Tiles.js';
 import global from '../styles/Global.module.css';
+import ffDash from '../assets/FF_Dash.png';
+import ffProf from '../assets/FF_Profile.png';
+import ffLoca from '../assets/FF_Location.png';
+import ffSoca from '../assets/FF_Social.png';
+import ffChart from '../assets/CS 407 Project Charter.pdf';
+import ffPlan1 from '../assets/CS407 Sprint 1 Planning Doc.pdf';
+import ffPlan2 from '../assets/CS407 Sprint 2 Planning Doc.pdf';
+import ffPlan3 from '../assets/CS407 Sprint 3 Planning Doc.pdf';
+import ffRetro1 from '../assets/CS407 Sprint 1 Retrospective.pdf';
+import ffRetro2 from '../assets/CS 407 Sprint 2 Retrospective.pdf';
+import ffRetro3 from '../assets/CS 407 Sprint 3 Retrospective.pdf';
+import mediCond from '../assets/medi_conditions.jpg';
+import mediPath from '../assets/medi_graph.jpg';
+import mediQues from '../assets/medi_question.jpg';
+import mediFeti from '../assets/medi_confetti.jpg';
 
 export default function Creations() {
     const content = {
         'FinanceFriend': {
             key: 'financefriend',
             date: 'Fall 2023',
-            desc: 'Senior Software Engineering Project at Purdue',
-            links: ["Github Repo:https://github.com/JohnRob1/FinanceFriend.git"]
+            desc: 'For Senior Software Engineering Project at Purdue, my team and I built a budget ' +
+                'application using the Flutter Web framework (in the Dart programming language) and Firebase. ' +
+                'My contributions to this project were the below features: \n' +
+                '-Account creation\n-Profile management\n-Account dashboard\n-Input spending by location\n' +
+                '-Database management\n-Social media posts\n-Spending goal comments\n',
+            pics: [ffDash, ffProf, ffLoca, ffSoca],
+            links: [
+                ["Github Repo", "https://github.com/JohnRob1/FinanceFriend.git"],
+                ["Project Charter", ffChart],
+                ["Sprint 1 Plan", ffPlan1],
+                ["Sprint 1 Retrospective", ffRetro1],
+                ["Sprint 2 Plan", ffPlan2],
+                ["Sprint 2 Retrospective", ffRetro2],
+                ["Sprint 3 Plan", ffPlan3],
+                ["Sprint 3 Retrospective", ffRetro3],
+            ]
         },
         'MediLingo': {
             key: 'medilingo',
             date: 'October 2023',
-            desc: 'My Project at the PittChallenge Hackathon',
-            links: ["Github Repo:https://github.com/JohnRob1/MediLingo",
-                "DevPost:https://devpost.com/software/medilingo"
+            desc: 'My Project at the PittChallenge Hackathon, where my team and I ' +
+                'developed a website using MongoDB, Typescript, Next.js, Node.js, Vite, and Clerk.' +
+                'Our goal for this website was to educate people about common medical ' +
+                'conditions. My contributions to this project are the graph component ' +
+                'that represents game progress and I made multiple React components that ' +
+                'my teammates were able to use in making the game components. We won the ' +
+                'best use of MongoDB Athlas and the Health Literacy category.',
+            pics: [mediCond, mediPath, mediQues, mediFeti],
+            links: [
+                ["Github Repo", "https://github.com/JohnRob1/MediLingo"],
+                ["DevPost", "https://devpost.com/software/medilingo"]
             ]
         },
         'JudgeMe': {
             key: 'judgeme',
             date: 'Fall 2022',
-            desc: 'Software Engineer Class Project at Purdue',
-            links: ["Github Repo:https://github.com/JohnRob1/JudgeMe"]
+            desc: 'For my Software Engineering Class Project at Purdue, my team and I built ' +
+                'a web application using the Django framework (Python) and TailwindCSS.',
+            links: [["Github Repo", "https://github.com/JohnRob1/JudgeMe"]]
         },
         'Messaging Application': {
             key: 'cs180project',
             date: 'Fall 2020',
             desc: 'Final Project in my Object Oriented Programming class at Purdue',
-            links: ["Github Repo:https://github.com/JohnRob1/Project5"]
+            links: [["Github Repo", "https://github.com/JohnRob1/Project5"]]
         },
         'Lot Tracking Application in Java': {
             key: 'lot-tracker',
             date: 'April 2022',
-            desc: 'Using the experience I got in Java from my Object Oriented Programming, I wanted to solve a problem I was facing at my part-time position at Fisher Packing. We were writing all of the lot numbers of ingredients by hand, so I sped up the process with lot-tracker!',
-            links: ["Github Repo:https://github.com/JohnRob1/Project5"]
+            desc: 'Using the experience I got in Java from my Object Oriented Programming, ' +
+                'I wanted to solve a problem I was facing at my part-time position at Fisher Packing. ' +
+                'We were writing all of the lot numbers of ingredients by hand, ' +
+                'so I sped up the process with lot-tracker!',
+            links: [["Github Repo", "https://github.com/JohnRob1/Project5"]]
         }
     };
 
