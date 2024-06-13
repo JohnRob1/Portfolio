49b1207bba195d7d2b0c33f093d5ed1cc433debf
JohnRob1 <robin557@purdue.edu>
2024-06-13 13:16:42 -0400

    set up routes

--- src/App.js
+++ src/App.js
@@ -4,22 +4,31 @@ import Home from './components/Home.js';
 import About from './components/About.js';
 import Creations from './components/Creations.js';
 import Experiences from './components/Experiences.js';
+import GradePredictor from './components/GradePredictor';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
     return (
-        <div className="App">
-            <header>
-                <CustomCursor />
-                <section>
-                    <div className="content">
-                        <About />
-                        <Experiences />
-                        <Creations />
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={
+                    <div className="App">
+                        <header>
+                            <CustomCursor />
+                            <section>
+                                <div className="content">
+                                    <About />
+                                    <Experiences />
+                                    <Creations />
+                                </div>
+                                <Home />
+                            </section>
+                        </header>
                     </div>
-                    <Home />
-                </section>
-            </header>
-        </div>
+                } />
+                <Route path='/grade-predictor' element={<GradePredictor />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
---
+++ src/components/GradePredictor.js
@@ -0,0 +1,7 @@
+import global from '../styles/Global.module.css';
+
+export default function GradePredictor() {
+    return (
+        <div className={global.title}> Grade Predictor </div>
+    );
+}
