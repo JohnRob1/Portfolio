6d28e49ae34bd11465024d876688a6b57e588ad2
JohnRob1 <robin557@purdue.edu>
2024-07-07 21:21:19 -0400

    styled the sign-in form, added TODOS

--- src/predictor_components/GradePredictor.js
+++ src/predictor_components/GradePredictor.js
@@ -2,8 +2,7 @@ import styles from '../predictor_styles/GradePredictor.module.css';
 import global from '../portfolio_styles/Global.module.css';
 import outputs from '../amplify_outputs.json';
 import { Amplify } from 'aws-amplify';
-import { signIn } from 'aws-amplify/auth'
-// import { generateClient } from 'aws-amplify/data';
+import { getCurrentUser, signIn, signUp } from 'aws-amplify/auth'
 import * as Images from '../portfolio_components/Global.js';
 import '../App.css';
 
@@ -26,38 +25,33 @@ function ClassLink({ name, pic }) {
 
 function ClassList() {
     return (
-        <div className={styles.classesOut}>
-            <div className={styles.classesIn}>
-                <h1 className={global.title}>
-                    Classes
-                </h1>
-                <div className={styles.classLinkContainer}>
-                    <ClassLink
-                        name='Test Class'
-                    />
-                    <ClassLink
-                        name='Test Class'
-                    />
-                    <ClassLink
-                        name='Test Class'
-                    />
-                </div>
-                <div className={styles.classLink}>
-                    <img
-                        className={styles.classAddPic}
-                        src={Images.addClass}
-                        alt={Images.addClass}
-                    />
-                </div>
+        <div className={global.container}>
+            <h1 className={global.title}>
+                Classes
+            </h1>
+            <div className={styles.classLinkContainer}>
+                <ClassLink
+                    name='Test Class'
+                />
+                <ClassLink
+                    name='Test Class'
+                />
+                <ClassLink
+                    name='Test Class'
+                />
             </div>
-        </div>
+            <div className={global.link} style={{ fontWeight: 'bolder' }}>
+                Add Class
+            </div>
+        </div >
     );
 }
 
+// TODO: validate inputs
 function Login() {
     async function handleSubmit(event) {
         event.preventDefault();
-        const form = event.currentTarget
+        const form = event.currentTarget;
         await signIn({
             username: form.elements.username.value,
             password: form.elements.password.value,
@@ -65,22 +59,25 @@ function Login() {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor='username'>Username:</label>
-            <input type='text' id='username' name='username' />
-            <label htmlFor='password'>Password:</label>
-            <input type='password' id='password' name='password' />
-            <input type='submit' />
-        </form>
+        <div className={global.container}>
+            <form onSubmit={handleSubmit} className={styles.login}>
+                <label> Want to save your data? </label>
+                <div className={styles.loginButtons}>
+                    <div className={global.link}>Sign In</div>
+                    <div className={global.link}>Sign Up</div>
+                </div>
+                <input type='text' id='username' name='username' placeholder='Username' />
+                <input type='password' id='password' name='password' placeholder='Password' />
+                <input type='submit' id='submit' />
+            </form>
+        </div>
     );
 }
 
 export default function GradePredictor() {
-    // const client = generateClient(); // use this Data client for CRUDL requests
-
     return (
         <div className="App">
-            <div className={styles.container}>
+            <div className={styles.main}>
                 <h1 className={global.title}>Predict My Grades</h1>
                 <ClassList />
                 <Login />
