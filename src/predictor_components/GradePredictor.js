087df97c66091739ffa86985802d4b917484c67b
JohnRob1 <robin557@purdue.edu>
2024-07-31 21:52:17 -0400

    redesigned class style and added some functionality. Need database to move on

--- src/predictor_components/GradePredictor.js
+++ src/predictor_components/GradePredictor.js
@@ -1,86 +1,68 @@
 import styles from '../predictor_styles/GradePredictor.module.css';
 import global from '../portfolio_styles/Global.module.css';
-import outputs from '../amplify_outputs.json';
+import outputs from "../amplify_outputs.json";
+import { Authenticator, useTheme, View } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
-import { signIn } from 'aws-amplify/auth'
-import * as Images from '../portfolio_components/Global.js';
+import { useState } from 'react';
+import '@aws-amplify/ui-react/styles.css';
 import '../App.css';
 
 Amplify.configure(outputs);
 
-function ClassLink({ name, pic }) {
-    return (
-        <div className={styles.classLink}>
-            <img
-                className={styles.classLinkPic}
-                src={pic ? pic : Images.classIcon}
-                alt={pic ? pic : Images.classIcon}
-            />
-            <div className={styles.classLinkName}>
-                {name}
-            </div>
-        </div>
-    );
-}
-
 function ClassList() {
+    const Class = () => {
+        return (
+            <div className={styles.classLink}>
+                <input className={styles.classAttrs} placeholder="Your Class" />
+            </div>
+        );
+    }
+
+    const [classList, setClassList] = useState([]);
+
+    const addClass = () => {
+        setClassList(classList.concat(<Class key={classList.length} />));
+    }
+
     return (
-        <div className={global.container}>
+        <div className={styles.classes}>
             <h1 className={global.title}>
                 Classes
             </h1>
-            <div className={styles.classLinkContainer}>
-                <ClassLink
-                    name='Test Class'
-                />
-                <ClassLink
-                    name='Test Class'
-                />
-                <ClassLink
-                    name='Test Class'
-                />
-            </div>
-            <div className={global.link} style={{ fontWeight: 'bolder' }}>
-                Add Class
+            <div className={styles.classLinksContainer}>
+                {classList}
+                <div
+                    className={global.link}
+                    style={{ fontWeight: 'bolder', width: '100%' }}
+                    onClick={addClass}
+                >
+                    Add Class
+                </div>
             </div>
         </div >
     );
 }
 
-// TODO: validate inputs
-function Login() {
-    async function handleSubmit(event) {
-        event.preventDefault();
-        const form = event.currentTarget;
-        await signIn({
-            username: form.elements.username.value,
-            password: form.elements.password.value,
-        })
+
+export default function GradePredictor() {
+
+    const header = {
+        Header() {
+            const { tokens } = useTheme();
+
+            return (
+                <View textAlign="center" padding={tokens.space.large}>
+                    <h1 style={{ color: 'var(--tertiary)' }}>Save Your Classes!</h1>
+                </View>
+            );
+        }
     }
 
     return (
-        <div className={global.container}>
-            <form onSubmit={handleSubmit} className={styles.login}>
-                <label> Want to save your data? </label>
-                <div className={styles.loginButtons}>
-                    <div className={global.link}>Sign In</div>
-                    <div className={global.link}>Sign Up</div>
-                </div>
-                <input type='text' id='username' name='username' placeholder='Username' />
-                <input type='password' id='password' name='password' placeholder='Password' />
-                <input type='submit' id='submit' />
-            </form>
-        </div>
-    );
-}
-
-export default function GradePredictor() {
-    return (
         <div className="App">
             <div className={styles.main}>
-                <h1 className={global.title}>Predict My Grades</h1>
                 <ClassList />
-                <Login />
+                <Authenticator components={header} />
             </div>
         </div>
     );
