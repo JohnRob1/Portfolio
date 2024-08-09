3a753927f5aae1adc5b0848db0e1d091e16db559
JohnRob1 <robin557@purdue.edu>
2024-08-09 15:13:51 -0400

    List deletion and state

--- src/predictor_components/GradeHeader.js
+++ src/predictor_components/GradeHeader.js
@@ -1,8 +1,9 @@
 import styles from '../predictor_styles/ClassList.module.css';
 import global from '../portfolio_styles/Global.module.css';
 
-const GradeHeader = ({ name, actual, total, onChange }) => {
+const Header = ({ id, name, actual, total, remove }) => {
     const grade = actual / total;
+    console.log(id);
 
     return (
         <div className={styles.classHeader} style={{ width: '100%' }}>
@@ -11,19 +12,23 @@ const GradeHeader = ({ name, actual, total, onChange }) => {
             </div>
             <div style={{ display: 'flex' }}>
                 <div className={styles.classAttrs}>
-                    {actual + ' / ' + total}
+                    {(actual + ' / ' + total)}
                 </div>
                 <div className={styles.classAttrs}>
-                    {grade + '%'}
+                    {grade ? grade : 0 + '%'}
                 </div>
-                <div
-                    className={global.link}
-                    style={{ color: 'var(--primary)', margin: '0px' }}>
-                    Delete
-                </div>
+                {id
+                ?   <div
+                        className={global.link}
+                        style={{ color: 'var(--primary)', margin: '0px' }}
+                        onClick={() => remove(id)}>
+                        Delete
+                    </div>
+                :   <></>
+                }
             </div>
         </div>
     );
 }
 
-export default GradeHeader;
+export default Header;
--- src/predictor_components/Weight.js
+++ src/predictor_components/Weight.js
@@ -3,27 +3,56 @@ import global from '../portfolio_styles/Global.module.css';
 import Header from './GradeHeader.js';
 import { useState } from 'react';
 
+function Sum(array) {return (array.reduce((sum, i) => sum + i))}
+const maxAssignments = 50;
+
 const Weight = () => {
     const [name, setName] = useState('Weight');
-    const [actual, setActual] = useState(0);
-    const [total, setTotal] = useState(100);
-    const [addAmount, setAddAmount] = useState(1);
-    const [assignments, setAssignments] = useState([<Header name='Assignment' actual={actual} total={total} />]);
-    const maxAssignments = 20;
 
-    const AddAssignment = () => {
+    const [actuals, setActuals] = useState([0]);
+    const [actual, setActual] = useState(Sum(actuals));
+
+    const [totals, setTotals] = useState([100]);
+    const [total, setTotal] = useState(Sum(totals));
+
+    const [addAmount, setAddAmount] = useState(1);
+    const [assignments, setAssignments] = useState([
+        { id: 0, name: 'Assignment', actual: actuals[0], total: total }
+    ]);
+
+    const removeAssignment = (id) => {
+        setAssignments(assignments.filter(a => a.id !== id));
+    }
+
+    const addAssignment = () => {
         setAssignments(prevAssignments => {
+            if (prevAssignments.length + addAmount > maxAssignments) return prevAssignments;
+
             const newAssignments = [...prevAssignments];
+            const assignmentTotal = ((total - Sum(totals)) / addAmount).toFixed(2);
+
             for (let i = 0; i < addAmount; i++) {
-                newAssignments.push(<Header key={assignments.length + i} />);
+                const id = assignments.length + i;
+                const newTotals = [...totals, assignmentTotal];
+                const newActuals = [...actuals, 0];
+                setTotals(newTotals);
+                setActuals(newActuals);
+
+                newAssignments.push({
+                    id: id,
+                    name: 'Assignment',
+                    actual: 0,
+                    total: assignmentTotal > 0 ? assignmentTotal : 0
+                });
             }
+
             return newAssignments;
         });
     }
 
     const AmountModifier = ({ num, more }) => {
         const ChangeAmount = () => {
-            if (more && addAmount <= (maxAssignments - num))
+            if (more && addAmount < (maxAssignments - num))
                 setAddAmount(addAmount + num)
             else if (!more && addAmount > (num))
                 setAddAmount(addAmount - num)
@@ -44,6 +73,43 @@ const Weight = () => {
         );
     }
 
+    const AddButton = () => {
+        if (assignments.length < maxAssignments) {
+            return (
+                <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
+                    <AmountModifier num={1} more={false} />
+                    <AmountModifier num={5} more={false} />
+                    <div
+                        className={global.link}
+                        style={{
+                            fontWeight: 'bolder',
+                            marginInline: '5px',
+                            color: 'var(--primary)',
+                            width: '80%',
+                        }}
+                        onClick={() => addAssignment()}>
+                        Add {addAmount} Assignment{addAmount > 1 ? 's' : ''}
+                    </div>
+                    <AmountModifier num={1} more={true} />
+                    <AmountModifier num={5} more={true} />
+                </div>
+            );
+        } else {
+            return (
+            <div style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    fontWeight: 'bolder',
+                    marginInline: '5px',
+                    color: 'var(--primary)',
+                    width: '100%'
+                }}>
+                Assignment Limit Reached
+            </div>
+            );
+        }
+    }
+
     return (
         <>
             <Header
@@ -52,24 +118,17 @@ const Weight = () => {
                 total={total}
             />
             <hr style={{ borderTop: '1px solid var(--primary)', width: '100%' }} />
-            {assignments}
-            <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
-                <AmountModifier num={1} more={false} />
-                <AmountModifier num={5} more={false} />
-                <div
-                    className={global.link}
-                    style={{
-                        fontWeight: 'bolder',
-                        marginInline: '5px',
-                        color: 'var(--primary)',
-                        width: '80%',
-                    }}
-                    onClick={() => AddAssignment()}>
-                    Add {addAmount} Assignment{addAmount > 1 ? 's' : ''}
-                </div>
-                <AmountModifier num={1} more={true} />
-                <AmountModifier num={5} more={true} />
-            </div >
+            {assignments.map(a => (
+                <Header
+                    key={a.id}
+                    id={a.id}
+                    name={a.name}
+                    actual={a.actual}
+                    total={a.total}
+                    remove={removeAssignment}
+                />
+            ))}
+            <AddButton />
         </>
     );
 }
